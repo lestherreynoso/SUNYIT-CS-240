1f42201a500cfd1656e3add14cc85b1eae1cad16
lestherreynoso <reynosl@sunyit.edu>
2014-05-21 03:27:46 -0400

    Update Lesther_Reynoso_GHP_7.cpp
    tidied it up a bit

--- Lesther_Reynoso_GHP_7.cpp
+++ Lesther_Reynoso_GHP_7.cpp
@@ -18,19 +18,19 @@ int main(void)
   string word; 
   string hasharray[23];
   count=0;
- cout<<"Enter 15 words to hash into an array"<<endl;
+  cout<<"Enter 15 words to hash into an array"<<endl;
  
- for (i=0; i<23; i++)
-  hasharray[i]="??";
+  for (i=0; i<23; i++)
+  	hasharray[i]="??";
 
- for (n=1; n<16; n++)
+ 	for (n=1; n<16; n++)
   { cout<<"Enter word "<<n<<":"<<endl;
     cin>>word;
     first_letter=word[0];
     last_letter=word[(word.length())-1];
     hv=(int(first_letter) + int(last_letter))%23;
     while(1)
-   { if (hv<23)
+    { if (hv<23)
       {  if (hasharray[hv] == "??")
          { hasharray[hv]=word;
            break;
@@ -38,46 +38,44 @@ int main(void)
          else
            hv++;
       }
-     else if (hv >= 23)
+    else if (hv >= 23)
        hv=1;
      } 
   } 
- cout<<endl;
- for (i=0; i<23; i++)
-   cout<<"Position "<<i<<":"<<hasharray[i]<<endl;
- while(1)
-{ cout<<"Enter a word to search for (999 to exit): ";
- cin>>word;
- if (word == "999")
-   break;
- else
-  { first_letter=word[0];
-    last_letter=word[(word.length())-1];
-    hv=(int(first_letter) + int(last_letter))%23;
-   while (1)
-   { if (hv < 23) 
-    { if (hasharray[hv]==word)
-       { cout<<"The word was found in position "<<hv<<endl;
-         break; 
-       }
-      else
-      {if (count==24)
-          { cout<<"The word you entered was not found"<<endl;
-            break;          
-          }
-       else
-       {
-         count++;
-         hv++;
-       }
-      }
-    }
-    else if (hv >= 23)
-      hv=1;
-   }
-  }
-}
-return 0;
-
-
+  
+ 	cout<<endl;
+ 	for (i=0; i<23; i++)
+  	cout<<"Position "<<i<<":"<<hasharray[i]<<endl;
+ 	while(1)
+	{ cout<<"Enter a word to search for (999 to exit): ";
+ 		cin>>word;
+ 		if (word == "999")
+	  	break;
+ 		else
+	 	{ first_letter=word[0];
+    	last_letter=word[(word.length())-1];
+    	hv=(int(first_letter) + int(last_letter))%23;
+   		while (1)
+   		{ if (hv < 23) 
+    		{ if (hasharray[hv]==word)
+       		{ cout<<"The word was found in position "<<hv<<endl;
+         		break; 
+       		}
+      		else
+      		{	if (count==24)
+          	{ cout<<"The word you entered was not found"<<endl;
+            	break;          
+          	}
+      			else
+	       		{	count++;
+         			hv++;
+       			}
+      		}
+    		}
+    	else if (hv >= 23)
+      	hv=1;
+   		}
+  	}
+	}
+	return 0;
 }
